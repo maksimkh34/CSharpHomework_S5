27ea0a40e6feadc690982ceee960f40fb9b4fb31
Maksim <epic.twod@mail.ru>
2022-06-22 10:42:13 +0300

    Task 3

---
+++ Z3/Program.cs
@@ -0,0 +1,44 @@
+﻿void fillarray(double[] array, double min, double max)
+{
+    Random rand = new Random();
+    for(int i = 0; i < array.Length; i++)
+    {
+        array[i] = rand.NextDouble();
+    }
+}
+
+void printarray(double[] array)
+{
+    for(int i = 0; i < array.Length - 1; i++)
+    {
+       Console.Write(array[i] + ", ");
+    }
+     Console.Write(array[array.Length - 1] + ". ");
+}
+
+double findmax(double[] array)
+{
+    double max = array[0];
+    for(int i = 1; i< array.Length; i++)
+    {
+        if(array[i] > max) max = array[i];
+    }
+    return max;
+}
+
+double findmin(double[] array)
+{
+    double min = array[0];
+    for(int i = 1; i< array.Length; i++)
+    {
+        if(array[i] < min) min = array[i];
+    }
+    return min;
+}
+Random rand = new Random();
+
+double[] array = new double[rand.Next(3, 8)];
+fillarray(array, 1, 20);
+printarray(array);
+
+Console.WriteLine("\n" + (findmax(array)-findmin(array)));
